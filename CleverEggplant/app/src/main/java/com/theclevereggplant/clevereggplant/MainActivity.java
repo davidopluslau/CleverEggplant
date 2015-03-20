67a90c14d6fa4e2f7b781554bb5f77f1ee9fa765
David Lau <david@refresh.io>
2015-03-19 18:25:20 -0700

    Shitty variable names are shitty

--- CleverEggplant/app/src/main/java/com/theclevereggplant/clevereggplant/MainActivity.java
+++ CleverEggplant/app/src/main/java/com/theclevereggplant/clevereggplant/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This is how we can grab a textview and change its text
+        TextView eggplant = (TextView) findViewById(R.id.lauBuiltThisShittyVariableName);
+        eggplant.setText("Truebar!");
     }
 
 
