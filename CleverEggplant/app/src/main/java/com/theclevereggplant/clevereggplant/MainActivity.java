1ad653e4c76081d54ce93dcaa68c665694cb96da
David Lau <david@refresh.io>
2015-03-24 10:52:42 -0700

    Restored menu bar to conform with modern style

--- CleverEggplant/app/src/main/java/com/theclevereggplant/clevereggplant/MainActivity.java
+++ CleverEggplant/app/src/main/java/com/theclevereggplant/clevereggplant/MainActivity.java
@@ -1,13 +1,13 @@
 package com.theclevereggplant.clevereggplant;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
     DemoPagerAdapter demoPagerAdapter;
     ViewPager viewPager;
 
@@ -15,6 +15,7 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle(R.string.menu_title);
 
         demoPagerAdapter = new DemoPagerAdapter(getSupportFragmentManager(), this);
         viewPager = (ViewPager) findViewById(R.id.pager);
